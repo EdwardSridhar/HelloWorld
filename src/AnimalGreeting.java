f9dd6977affeb14c59335f5b80cd9c14e5bb2010
Edward Sriidhar <edwardwaltham@gmail.com>
2020-09-10 12:23:54 -0700

    Finished animal greeting

--- src/AnimalGreeting.java
+++ src/AnimalGreeting.java
@@ -1,5 +1,12 @@
 public class AnimalGreeting {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("|￣￣￣￣￣￣|");
+        System.out.println("| HELLO    |");
+        System.out.println("| FELLOW   |");
+        System.out.println("| CODER    |");
+        System.out.println("|＿＿＿＿＿__|");
+        System.out.println(" (\\__/) ||");
+        System.out.println(" (•ㅅ•) ||");
+        System.out.println(" /  　 づ||");
     }
 }
